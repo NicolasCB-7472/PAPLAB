9f3114f38a2b66cd2ec212407aa1bdab1ad86c58
Nicolas <bardanca.nicolas@gmail.com>
2025-08-23 15:25:08 -0300

    Se agregan las dependencias de jakarta al proyecto, para la persistencia

---
+++ lectores/src/main/java/persistencia/Conexion.java
@@ -0,0 +1,9 @@
+package persistencia;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Conexion {
+    
+}
