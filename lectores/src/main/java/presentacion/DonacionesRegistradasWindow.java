fc8331a25e10c09ccc38152cd8f9ec02eb08e5c0
Matias Ronca <86140427+matironca@users.noreply.github.com>
2025-09-01 09:23:50 -0300

    Hacer que se impriman los datos de las clases derivadas al ver las donaciones registradas.

--- lectores/src/main/java/presentacion/DonacionesRegistradasWindow.java
+++ lectores/src/main/java/presentacion/DonacionesRegistradasWindow.java
@@ -1,7 +1,6 @@
 package presentacion;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -11,9 +10,10 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-import interfaces.IControlador;
+import datatypes.DtArticulo;
+import datatypes.DtLibro;
 import datatypes.DtMaterial;
-
+import interfaces.IControlador;
 public class DonacionesRegistradasWindow {
     
     private IControlador controlador;
@@ -68,6 +68,17 @@ public class DonacionesRegistradasWindow {
             for (DtMaterial material : materiales) {
                 contenido.append("ID: ").append(material.getId()).append("\n");
                 contenido.append("Fecha de ingreso: ").append(material.getFechaIngreso()).append("\n");
+                if(material instanceof DtLibro){
+                    DtLibro libro  = (DtLibro) material;
+                    contenido.append("Titulo: ").append(libro.getTitulo()).append("\n");
+                    contenido.append("Cantidad de paginas: ").append(libro.getCantPaginas()).append("\n");
+                }
+                else if(material instanceof DtArticulo){
+                    DtArticulo art = (DtArticulo) material;
+                    contenido.append("Descripcion: ").append(art.getDescripcion()).append("\n");
+                    contenido.append("Dimensiones: ").append(art.getDimensiones()).append("\n");
+                    contenido.append("Peso: ").append(art.getPeso()).append("\n");
+                }
                 contenido.append("----------------------------------------\n");
             }
             
